de90737d58677f2a49c205912586179695c76904
AgataKondzior <ak.kondzior@gmail.com>
2017-05-10 22:13:47 +0200

    add

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+     $('.fruit').on('click','img',function(){
+        $(this).closest('.fruit').find('h3').slideToggle();  
+    });  
+    $('#kilo').on('change',function(){
+        var price = +$(this).closest('.fruit').data('price');
+        var quantity = +$(this).val();   
+        $('#total').text(price * quantity);
+    });
+});
+      
+
+
+    
+
+ 
+ 
+
+
