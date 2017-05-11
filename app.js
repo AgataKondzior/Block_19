1b87f9813a084f6717417be522ff6dd90bb15be8
AgataKondzior <ak.kondzior@gmail.com>
2017-05-11 15:58:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
      $('.fruit').on('mouseenter','img',function(){
         $(this).closest('.fruit').find('h3').slideToggle();  
     });  
-    $('#weight').on('change',function(){
+    $('.weight').on('change',function(){
         var price = +$(this).closest('.fruit').data('price');
         var quantity = +$(this).val();   
         $('.total').text(price * quantity);
