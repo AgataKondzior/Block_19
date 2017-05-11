b69dd963eaa1dd55e07aadef5972aa04fcf322b3
AgataKondzior <ak.kondzior@gmail.com>
2017-05-11 18:16:53 +0200

    add

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-     $('.fruit').on('mouseenter','img',function(){
-        $(this).closest('.fruit').find('h3').slideToggle();  
+$(document).ready(function () {
+    $('.fruit').on('mouseenter', 'img', function() {
+        $(this).closest('.fruit').find('h3').slideToggle();
     });  
-    $('.weight').on('change',function(){
+    $('.weight').on('change', function () {
         var price = +$(this).closest('.fruit').data('price');
         var quantity = +$(this).val();   
-        $('.total').text(price * quantity);
+        $(this).closest('.total').text(price * quantity);
+        
     });
 });
       
