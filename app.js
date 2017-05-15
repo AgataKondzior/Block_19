9081f6ce23f9375b7a9c03bdc032d66d4c109dc7
AgataKondzior <ak.kondzior@gmail.com>
2017-05-15 10:52:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('.weight').on('change', function () {
         var price = +$(this).closest('.fruit').data('price');
         var quantity = +$(this).val(); 
-    $(this).closest('.fruit')[0].querySelector('.total').innerHTML=price * quantity;
+    $(this).closest('.fruit')[0].find('.total').text('price * quantity');
      
      
     });
