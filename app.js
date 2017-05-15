622ea540227865f71ea9bf29327eadf442ec6801
AgataKondzior <ak.kondzior@gmail.com>
2017-05-15 10:53:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('.weight').on('change', function () {
         var price = +$(this).closest('.fruit').data('price');
         var quantity = +$(this).val(); 
-    $(this).closest('.fruit')[0].find('.total').text('price * quantity');
+    $(this).closest('.fruit').find('.total').text('price * quantity');
      
      
     });
